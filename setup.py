7218ba432bd252417d67ccf3e6897827c7dd7c34
uditjuneja <uditjuneja1@gmail.com>
2022-04-15 11:02:05 +0530

    wip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="baseplate",
+    version="0.0.1",
+    description="Baseplate",
+    url="#",
+    author="Mudrex Platform Team",
+    author_email="platform@mudrex.com",
+    packages=find_packages(),
+    zip_safe=False,
+)
